e58c8c6a824f94531bece3e12ce1cc99f310f791
Lukas W <wendzel42@gmail.com>
2026-05-08 16:33:44 +0200

    seting up Vehicle class for storing basic data

--- publictransport/src/main/java/cz/cuni/mff/java/Vehicle.java
+++ publictransport/src/main/java/cz/cuni/mff/java/Vehicle.java
@@ -1,5 +1,47 @@
 package cz.cuni.mff.java;
 
 public class Vehicle {
-    
+    private int id;
+    private VehicleTypes type;
+    private String model;
+    private int capacity;
+    private String history;
+    private int pricePerKm;
+    public Vehicle(int id, VehicleTypes type, int capacity, String history, int pricePerKm, String model) {
+        this.id = id;
+        this.type = type;
+        this.capacity = capacity;
+        this.model = model;
+        this.history = history;
+        this.pricePerKm = pricePerKm;
+    }
+    public Vehicle(int id, VehicleTypes type, int capacity, String history, int pricePerKm) {
+        this.id = id;
+        this.type = type;
+        this.capacity = capacity;
+        this.model = "Unknown";
+        this.history = history;
+        this.pricePerKm = pricePerKm;
+    }
+    public void addHistory(String history) {
+        this.history += history;
+    }
+    public int getId() {
+        return id;
+    }
+    public VehicleTypes getType() {
+        return type;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getHistory() {
+        return history;
+    }
+    public int getPricePerKm() {
+        return pricePerKm;
+    }
 }

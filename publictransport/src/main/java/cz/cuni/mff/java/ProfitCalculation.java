fa561745489c4d38981e4abda0b9cd03de4cb4fd
Lukas W <wendzel42@gmail.com>
2026-05-14 22:42:15 +0200

    implementing profit calculation from simulation data

--- publictransport/src/main/java/cz/cuni/mff/java/ProfitCalculation.java
+++ publictransport/src/main/java/cz/cuni/mff/java/ProfitCalculation.java
@@ -1,10 +1,19 @@
 package cz.cuni.mff.java;
 
 public class ProfitCalculation {
-    public double calculateBalanceAterSimulation(VehicleInSimulation vehicles, PassengersStatistics statistics){ // vehicles has stored info about journeys
-        int numberOfTransportedPassagers = statistics.totalPassengersTransported();
-
+    int numberOfTransportedPassagers = 0;
+    int averagePriceOfTicket = 20;
+    double moneyMade = 0;
+    double moneySpent = 0;
+    double profit;
+    public double calculateBalanceAterSimulation(VehicleInSimulation [] vehicles){ // vehicles has stored info about journeys
+        for (VehicleInSimulation vehicle : vehicles) {
+            numberOfTransportedPassagers += vehicle.getNumberOfTransportedPassagers();
+            moneySpent += vehicle.getDistanceTraveledMeters() * vehicle.getPricePerKm() /1000;
+        }
+        moneyMade = numberOfTransportedPassagers * averagePriceOfTicket;
+        profit = moneyMade - moneySpent;
+        return profit;
         
-        return 1.2;
     }
 }
